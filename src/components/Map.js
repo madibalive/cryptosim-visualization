e4f3b6ac25bcb4958e923fce23b4d2346e9c7af0
Gil Shotan <gilsho@cs.stanford.edu>
2021-05-14 04:13:35 +0300

    refactor map intitialization into separate functions

--- src/components/Map.js
+++ src/components/Map.js
@@ -28,98 +28,105 @@ class Map extends React.Component {
 
     this.map.on('load', function () {
       this.map.resize();
-
-      this.map.addImage(
-        'pulsing-dot', new PulsingDot(100, this.map), { pixelRatio: 2 });
-
-      this.map.addSource('satellites', {
-        'type': 'geojson',
-        'data': {
-          'type': 'FeatureCollection',
-          'features': [
-            {
-              'type': 'Feature',
-              'geometry': {
-                'type': 'Point',
-                'coordinates': [0, 0]
-              }
-            }
-          ]
-        }
-      });
-    
-      this.map.addLayer({
-        'id': 'satelliteLayer',
-        'type': 'symbol',
-        'source': 'satellites',
-        'layout': {
-          'icon-image': 'pulsing-dot'
-        }
-      });
-
-      this.map.addSource('groundStations', {
-        'type': 'geojson',
-        'data': {
-          'type': 'FeatureCollection',
-          'features': [
-            {
-              'type': 'Feature',
-              'geometry': {
-                'type': 'Point',
-                'coordinates': [0, 0]
-              }
-            }
-          ]
-        }
-      });
-
-      this.map.addLayer({
-        'id': 'groundStationLayer',
-        'type': 'circle',
-        'source': 'groundStations',
-        'paint': {
-          'circle-radius': 5,
-          'circle-color': '#5b94c6',
-        }
-      });
-
-      // Create a popup for displaying ground station info
-      let popup = new mapboxgl.Popup({
-        closeButton: false,
-        closeOnClick: false,
-      });
-
-      this.map.on('mouseenter', 'groundStationLayer', (e) => {
-        // Change the cursor style as a UI indicator.
-        this.map.getCanvas().style.cursor = 'pointer';
-
-        let coordinates = e.features[0].geometry.coordinates.slice();
-        let description = e.features[0].properties.id;
-
-        // Ensure that if the map is zoomed out such that multiple
-        // copies of the feature are visible, the popup appears
-        // over the copy being pointed to.
-        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-        }
-
-        // Populate the popup and set its coordinates
-        // based on the feature found.
-        popup.setLngLat(coordinates).setHTML(description).addTo(this.map);
-      });
-
-      this.map.on('mouseleave', 'groundStationLayer', () => {
-        this.map.getCanvas().style.cursor = '';
-        popup.remove();
-      });
-
+      this.setupStations();
+      this.setupSatellites();
+      this.setupStationPopup();
       this.animate();
-    }.bind(this));  
+    }.bind(this));
   }
 
   animate() {
     this.draw();
     requestAnimationFrame(this.animate.bind(this));
+  }
+
+  setupStations() {
+    this.map.addSource('groundStations', {
+      'type': 'geojson',
+      'data': {
+        'type': 'FeatureCollection',
+        'features': [
+          {
+            'type': 'Feature',
+            'geometry': {
+              'type': 'Point',
+              'coordinates': [0, 0]
+            }
+          }
+        ]
+      }
+    });
+
+    this.map.addLayer({
+      'id': 'groundStationLayer',
+      'type': 'circle',
+      'source': 'groundStations',
+      'paint': {
+        'circle-radius': 5,
+        'circle-color': '#5b94c6',
+      }
+    });
+  }
+
+  setupSatellites() {
+    this.map.addImage(
+      'pulsing-dot', new PulsingDot(100, this.map), { pixelRatio: 2 });
+
+    this.map.addSource('satellites', {
+      'type': 'geojson',
+      'data': {
+        'type': 'FeatureCollection',
+        'features': [
+          {
+            'type': 'Feature',
+            'geometry': {
+              'type': 'Point',
+              'coordinates': [0, 0]
+            }
+          }
+        ]
+      }
+    });
+    this.map.addLayer({
+      'id': 'satelliteLayer',
+      'type': 'symbol',
+      'source': 'satellites',
+      'layout': {
+        'icon-image': 'pulsing-dot'
+      }
+    });
+  }
+
+  setupStationPopup() {
+    // Create a popup for displaying ground station info
+    let popup = new mapboxgl.Popup({
+      closeButton: false,
+      closeOnClick: false,
+    });
+
+    this.map.on('mouseenter', 'groundStationLayer', (e) => {
+      // Change the cursor style as a UI indicator.
+      this.map.getCanvas().style.cursor = 'pointer';
+
+      let coordinates = e.features[0].geometry.coordinates.slice();
+      let description = e.features[0].properties.id;
+
+      // Ensure that if the map is zoomed out such that multiple
+      // copies of the feature are visible, the popup appears
+      // over the copy being pointed to.
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+
+      // Populate the popup and set its coordinates
+      // based on the feature found.
+      popup.setLngLat(coordinates).setHTML(description).addTo(this.map);
+    });
+
+    this.map.on('mouseleave', 'groundStationLayer', () => {
+      this.map.getCanvas().style.cursor = '';
+      popup.remove();
+    });
   }
 
   draw() {
