f3d332003ce7740a9362a31556a80d44a8197fb7
Gil Shotan <gilsho@cs.stanford.edu>
2021-06-18 14:17:54 +0300

    s/CoverageArea/SatelliteCoverageArea

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Map from './components/Map';
 import ControlBar from './components/ControlBar';
-import CoverageArea from './components/CoverageArea';
+import SatelliteCoverageArea from './components/SatelliteCoverageArea';
 import SatelliteInfoBar from './components/SatelliteInfoBar';
 import Trajectory from './components/Trajectory';
 import {universe, gsnetwork} from './demo';
@@ -33,7 +33,7 @@ class App extends React.Component {
   render() {
     const sat = universe.satellites().get(this.state.satelliteId);
     let trajectory = this.state.displayTrajectory ? <Trajectory satellite={sat}/> : null;
-    let coverage = this.state.displayCoverage ? <CoverageArea satellite={sat}/> : null;
+    let coverage = this.state.displayCoverage ? <SatelliteCoverageArea satellite={sat}/> : null;
     return (
       <div className="App">
         <ControlBar universe={universe}
