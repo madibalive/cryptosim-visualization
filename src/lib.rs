4909117687e561c44effe829e13ac8102de53b47
Yan Michalevsky <yan@cryptosat.io>
2022-12-03 13:22:43 -0800

    Refactored Ballot into a class with methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 use wasm_bindgen::prelude::*;
 use rsa::{PublicKey, RsaPrivateKey, RsaPublicKey, PaddingScheme, 
           pkcs8::EncodePublicKey, pkcs8::LineEnding};
+use std::collections::HashMap;          
 
+#[wasm_bindgen]
 pub struct Ballot {
-    k       : usize,          // k-anonymity parameter
+    k       : u32,          // k-anonymity parameter
     privkey : RsaPrivateKey,  // private key in PKCS8 PEM encoding
-    pubkey  : RsaPublicKey    // public key in PKCS8 PEM encoding
+    pubkey  : RsaPublicKey,   // public key in PKCS8 PEM encoding
+    votes   : Vec<String>     // list of submitted votes 
 }
 
 #[cfg(target_family="wasm")]
@@ -28,43 +31,62 @@ fn gen_keypair() -> (RsaPublicKey, RsaPrivateKey) {
     return (pubkey, privkey)
 }
 
-fn init_ballot_internal(_k: usize) -> Ballot {
-    let (_pubkey, _privkey) = gen_keypair();
-    return Ballot {k: _k, privkey: _privkey, pubkey: _pubkey}
-}
-
-/// Returns the public key for a given ballot encoded in PEM (PKCS8) format.
-/// ballot - pointer to the Ballot object to extract the public key for
 #[wasm_bindgen]
-pub fn get_ballot_pubkey_pem(ballot: *const Ballot) -> String {
-    unsafe {
-        return (*ballot).pubkey.to_public_key_pem(LineEnding::default()).unwrap();
+impl Ballot {
+
+    /// Initializes a new ballot.
+    /// k - the k-anonymity parameter
+    pub fn new(k: u32) -> Ballot {
+        let (_pubkey, _privkey) = gen_keypair();
+
+        log(format!("Ballot initialized with k={}", k));
+
+        Ballot {
+            k: k,
+            privkey: _privkey,
+            pubkey: _pubkey,
+            votes: Vec::new()
+        }
     }
+
+    /// Returns the public key for a given ballot encoded in PEM (PKCS8) format.
+    /// ballot - pointer to the Ballot object to extract the public key for
+    pub fn get_pubkey_pem(&self) -> String {
+        return self.pubkey.to_public_key_pem(LineEnding::default()).unwrap();
+    }
+
+    /// Submit an encrypted vote.
+    /// encrypted_vote - encrypted vote encoded in Base64
+    pub fn vote(&mut self, encrypted_vote: String) {
+        log(format!("Received a new vote: {}", encrypted_vote));
+        
+        self.votes.push(encrypted_vote);
+        log(format!("Added vote to list"));
+    }
+
+    /// Finalizes the ballot in order to reveal the results.
+    pub fn finalize(&self) -> String {
+        let total_votes = self.votes.len();
+        log(format!("Received {} votes in total", total_votes));
+        
+        if total_votes < self.k as usize {
+            log(format!("Received less votes than minimum quorum size. Cannot finalize ballot."));
+            return "".to_owned()
+        }
+
+        let mut vote_count : HashMap<String, usize> = HashMap::new();
+        for vote in &self.votes {
+            vote_count.entry(vote.to_owned()).and_modify(|count| *count += 1).or_insert(1);
+        }
+
+        // create a sorted votes vector by count
+        let mut count_vec : Vec<_> = vote_count.iter().collect();
+        count_vec.sort_by(|a, b| b.1.cmp(a.1));
+        return count_vec[0].0.to_owned()
+    }
+
 }
 
-/**
-  Initializes a new ballot.
-  k - the k-anonymity parameter
-*/
-#[wasm_bindgen]
-pub fn init_ballot(k: u32) -> *const Ballot {
-    let ballot = init_ballot_internal(k as usize);
-    log(format!("Ballot initialized with k={}", k));
-    return &ballot;
-} 
-
-/// Submit an encrypted vote.
-/// encrypted_vote - encrypted vote encoded in Base64
-#[wasm_bindgen]
-pub fn vote(encrypted_vote: String) {
-
-}
-
-/// Finalizes the ballot in order to reveal the results.
-#[wasm_bindgen]
-pub fn finalize_ballot(ballot: *const Ballot) {
-    
-}
 
 #[cfg(test)]
 mod tests {
@@ -73,18 +95,38 @@ mod tests {
     #[test]
     fn test_gen_keypair() {
         let (_pubkey, _privkey) = gen_keypair();
-        assert!(true);
-    }
-
-    #[test]
-    fn test_init_ballot_internal() {
-        let _ballot = init_ballot_internal(1);
-        assert!(true);
     }
 
     #[test]
     fn test_init_ballot() {
-        let _ballot = init_ballot(1);
-        assert!(!_ballot.is_null());
+        let _ballot = Ballot::new(1);
     }
+
+    #[test]
+    fn test_get_pubkey_pem() {
+        let _ballot = Ballot::new(1);
+        let _pubkey_pem = _ballot.get_pubkey_pem();
+    }
+
+    #[test]
+    fn test_vote() {
+        let mut _ballot = Ballot::new(1);
+        _ballot.vote("test".to_owned());
+    }
+
+    #[test]
+    fn test_finalize_ballot() {
+        let mut _ballot = Ballot::new(1);
+        _ballot.vote("test".to_owned());
+        let winner = _ballot.finalize();
+        assert!(winner == "test");
+        log(format!("Ballot winner: {}", winner))
+    }
+
+    #[test]
+    fn test_finalize_less_than_k() {
+        let _ballot = Ballot::new(1);
+        _ballot.finalize();
+    }
+    
 }
--- tests/web.rs
+++ tests/web.rs
@@ -1,12 +1,31 @@
 #[path="../src/lib.rs"]
 mod private_voting;
 
+use private_voting::*;
+
 use wasm_bindgen_test::{wasm_bindgen_test,wasm_bindgen_test_configure};
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn test_init_ballot() {
-    let _ballot = private_voting::init_ballot(1);
-    assert!(!_ballot.is_null())
+    let _ballot = Ballot::new(1);
+}
+
+#[wasm_bindgen_test]
+fn test_get_pubkey_pem() {
+    let _ballot = Ballot::new(1);
+    let _pubkey_pem = _ballot.get_pubkey_pem();
+}
+
+#[wasm_bindgen_test]
+fn test_vote() {
+    let mut _ballot = Ballot::new(1);
+    _ballot.vote("test".to_owned());
+}   
+
+#[wasm_bindgen_test]
+fn test_finalize_ballot() {
+    let _ballot = Ballot::new(1);
+    _ballot.finalize();
 }
